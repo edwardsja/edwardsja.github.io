0a2d944f597f33c66b90e3605a33ca4c94bbf81d
Aaron Edwards <edw15013@byui.edu>
2019-09-21 16:06:17 -0500

    added current year

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,7 @@
 var modifiedDate = "";
 function getModifiedDate() {
     var modifiedDate = document.lastModified;
+    document.getElementById("currentYear").innerHTML = new Date().getFullYear();
     document.getElementById("lastModified").innerHTML = "Last Modified: " + modifiedDate;
     document.getElementById("lastModified").style.fontSize = ".85em";
 }
