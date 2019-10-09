aaa2055b81f6623263732e27db234d12eced39e0
Aaron Edwards <edw15013@byui.edu>
2019-10-08 22:23:43 -0500

    Minor visual fixes

--- lesson3/js/myscript.js
+++ lesson3/js/myscript.js
@@ -1,9 +1,5 @@
 var modifiedDate = "";
-function getModifiedDate() {
-    var modifiedDate = document.lastModified;
-    document.getElementById("currentYear").innerHTML = new Date().getFullYear();
-    document.getElementById("lastModified").innerHTML = "Last Modified: " + modifiedDate;
-    document.getElementById("lastModified").style.fontSize = ".85em";
-}
-
-window.onload = getModifiedDate;  
+var modifiedDate = document.lastModified;
+document.getElementById("currentYear").innerHTML = new Date().getFullYear();
+document.getElementById("lastModified").innerHTML = "Last Modified: " + modifiedDate;
+document.getElementById("lastModified").style.fontSize = ".85em";
