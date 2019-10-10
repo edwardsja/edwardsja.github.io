5f2a53bd023608d96016df15437b76a3681d78e5
Aaron Edwards <edw15013@byui.edu>
2019-10-09 22:43:35 -0500

    Validation fix

--- lesson3/js/myscript.js
+++ lesson3/js/myscript.js
@@ -3,3 +3,4 @@ var modifiedDate = document.lastModified;
 document.getElementById("currentYear").innerHTML = new Date().getFullYear();
 document.getElementById("lastModified").innerHTML = "Last Modified: " + modifiedDate;
 document.getElementById("lastModified").style.fontSize = ".85em";
+document.getElementById("lastModified").style.color = "#000000";
