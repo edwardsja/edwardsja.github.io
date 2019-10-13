cca4b4fd633cd8f0a3112738ff37308ffd5c2e1a
Aaron Edwards <edw15013@byui.edu>
2019-10-13 01:48:01 -0500

    W04 Assignment Done

---
+++ lesson4/js/myscript.js
@@ -0,0 +1,21 @@
+var modifiedDate = "";
+var modifiedDate = document.lastModified;
+document.getElementById("currentYear").innerHTML = new Date().getFullYear();
+document.getElementById("lastModified").innerHTML = "Last Modified: " + modifiedDate;
+document.getElementById("lastModified").style.fontSize = ".85em";
+document.getElementById("lastModified").style.color = "#000000";
+
+var i = 0;
+
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+
+    if (i == 0) {
+    document.getElementById("button").innerHTML = "X";
+    i -= 1;
+    }
+    else {
+        document.getElementById("button").innerHTML = "&#9776; Menu";
+    i += 1;
+    }
+}
